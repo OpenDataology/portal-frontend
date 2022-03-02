86f6c646290fdd5dec5f356c35856f9e186f7384
Claimonx <claimonx@gmail.com>
2022-03-02 23:28:21 +0800

    Hide interface

--- config/api.env.js
+++ config/api.env.js
@@ -1,7 +1,7 @@
-import axios from "./http.env"
-import { Token } from "./http.env"
+import axios from "./https.env"
+import { Token } from "./https.env"
 /**
- * 获取所有的License数据
+ * 
  * @param {String} url 
  * @param {Object} parms 
  * @returns 
@@ -18,8 +18,91 @@ export const getLicenseDataAll = (data = {}) => {
     })
 }
 
+
 /**
- * 获取所有的Dataset数据
+ * 
+ * @param {object} data 
+ * @returns 
+ */
+ export const getLicenseBasicInfo = (data) => {
+    return axios.request({
+        url: "/get_license_basic_by_id",
+        method: "get",
+        // params: data
+        params: {
+           id: data.id,
+           token: Token,
+        },
+    })
+}
+
+/**
+ * 
+ * @param {object} data 
+ * @returns 
+ */
+export const getLicenseBasicDataTab = (data) => {
+    return axios.request({
+        url: "/get_license_data_by_id",
+        method: "get",
+        // params: data
+        params: {
+            id: data.id,
+            token: Token,
+         },
+    })
+}
+
+/**
+ * 
+ * @param {object} data 
+ * @returns 
+ */
+export const getLicenseBasicModleTab = (data) => {
+    return axios.request({
+        url: "/get_license_model_by_id",
+        method: "get",
+        params: {
+            id: data.id,
+            token: Token,
+         },
+    })
+}
+
+/**
+ * 
+ * @param {object} data 
+ * @returns 
+ */
+export const getLicenseBasicOtherTab = (data) => {
+    return axios.request({
+        url: "/get_license_other_by_id",
+        method: "get",
+        params: {
+            id: data.id,
+            token: Token,
+         },
+    })
+}
+
+/**
+ * 
+ * @param {object} data 
+ * @returns 
+ */
+export const getLoadLicenseAll = (data) => {
+    return axios.request({
+        url: "/data-license",
+        method: "get",
+        params: {
+            data: data,
+            token: Token
+
+        }
+    })
+}
+/**
+ * 
  * @param {String} url 
  * @param {Object} parms 
  * @returns 
@@ -47,78 +130,10 @@ export const getDatasetBasic = (data) => {
 }
 
 
-/**
- * 
- * @param {object} data 
- * @returns 
- */
-export const getLicenseBasicInfo = (data) => {
-    return axios.request({
-        url: "/get_license_basic_by_id",
-        method: "get",
-        params: data
-    })
-}
+
 
 /**
  * 
- * @param {object} data 
- * @returns 
- */
-export const getLicenseBasicDataTab = (data) => {
-    return axios.request({
-        url: "/get_license_data_by_id",
-        method: "get",
-        params: data
-    })
-}
-
-/**
- * 
- * @param {object} data 
- * @returns 
- */
-export const getLicenseBasicModleTab = (data) => {
-    return axios.request({
-        url: "/get_license_model_by_id",
-        method: "get",
-        params: data
-    })
-}
-
-/**
- * 
- * @param {object} data 
- * @returns 
- */
-export const getLicenseBasicOtherTab = (data) => {
-    return axios.request({
-        url: "/get_license_other_by_id",
-        method: "get",
-        params: data
-    })
-}
-
-/**
- * 
- * @param {object} data 
- * @returns 
- */
-export const getLoadLicenseAll = (data) => {
-    return axios.request({
-        url: "/data-license",
-        method: "get",
-        params: {
-            data: data,
-            token: Token
-
-        }
-    })
-}
-
-
-/**
- * 获取所有的Dataset数据
  * @param {String} url 
  * @param {Object} parms 
  * @returns 
@@ -129,7 +144,7 @@ export const getLoadDatasetAll = (data) => {
         method: "get",
         params: {
             data: data,
-            token: '2f6b61b197c846f2c3da8efea0b37b6d3353d7fbc8acaca0ff9f03d3980794ec',
+            token: Token,
 
         }
     })
